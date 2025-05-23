ee0848fd00257dd636cca6cebb56354f7965d592
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2025-05-23 23:01:41 +0200

    Added: swipe function

--- script/script.js
+++ script/script.js
@@ -1,1 +1,23 @@
-console.log('Prova');
+let touchStartY = 0;
+let touchEndY = 0;
+const threshold = 50; // distanza minima per considerare swipe
+
+function handleGesture() {
+  if (touchEndY < touchStartY - threshold) {
+    console.log('Swipe verso l\'alto');
+    // Azione swipe up
+  }
+  if (touchEndY > touchStartY + threshold) {
+    console.log('Swipe verso il basso');
+    // Azione swipe down
+  }
+}
+
+document.addEventListener('touchstart', e => {
+  touchStartY = e.changedTouches[0].screenY;
+});
+
+document.addEventListener('touchend', e => {
+  touchEndY = e.changedTouches[0].screenY;
+  handleGesture();
+});
