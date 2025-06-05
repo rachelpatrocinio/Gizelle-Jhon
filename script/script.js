352635b608a9cacc86cc3a824f77dffd457bacb9
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2025-06-05 21:10:31 +0200

    nav2

--- script/script.js
+++ script/script.js
@@ -179,7 +179,7 @@ setInterval(updateCountdown, 1000);
 
 // === CLICK MENU ===
 function goToPage(page, mobile) {
-  var body = document.querySelector(body);
+  var body = document.querySelector('body');
   body.classList.remove('menu-open');
   var hamburgerMenu = document.querySelector('.menu-mobile');
   if(hamburgerMenu){
@@ -224,6 +224,6 @@ function openHamburgeMenu(){
     hamburgerMenu.classList.remove('d-none');
     hamburgerMenu.classList.add('d-block');
   }
-  var body = document.querySelector(body);
+  var body = document.querySelector('body');
   body.classList.add('menu-open');
 }
